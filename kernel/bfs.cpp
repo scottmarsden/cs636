8bad20b100ffa0124f1e34e67f31a9a22e886d69
scttmars <55801303+scttmars@users.noreply.github.com>
2021-10-14 11:30:53 -0400

    Update bfs.cpp

--- kernel/bfs.cpp
+++ kernel/bfs.cpp
@@ -10,30 +10,30 @@ using std::endl;
 void mtBFS(graph_t& g, vid_t root){
 	cout << "parallel BFS root = " << root << endl;
 	csr_t* csr = &g.csr;
-    	csr_t* csc = &g.csc;
+  csr_t* csc = &g.csc;
 	vid_t* nbrs = csr->get_nbrs();
-    	vid_t* offset = csr->get_offset();
+  vid_t* offset = csr->get_offset();
 	int visited[(int) csr->get_vcount()];
-    for (int i = 0; i < (int) csr->get_vcount(); i++){
+  for (int i = 0; i < (int) csr->get_vcount(); i++){
       visited[i] = 1000;
-    }
-    visited[root] = 0;
-	
-    bool complete = false;
-    omp_set_num_threads((int) csr->get_vcount());
-    vid_t level = 0;
-    
+  }
+  visited[root] = 0;
+
+  bool complete = false;
+  omp_set_num_threads((int) csr->get_vcount());
+  vid_t level = 0;
+
 	while (complete != true) {
   	//paralell loop instead of queue otherwise same structure as BFS
   	#pragma omp parallel for
   	for (int i = 0; i < (int) csr->get_vcount(); i++) {
     		if(visited[i] == level){
-			
+
 		vid_t start = offset[i];
       		vid_t end = offset[i + 1];
 		}
 	}
-		
+
 		for (int j = start; j < end; j++){
 			if (visited[nbrs[i]] > visited[currNode] + 1 ){
         		//cout << nbrs[i]  << endl;
@@ -49,7 +49,7 @@ void mtBFS(graph_t& g, vid_t root){
 		}
 	}
 	}
-	     
+
     //same print structure are regular BFS
     int levelCount = 0;
     for(int i = 0; i < (int) csr->get_vcount(); i++){
@@ -58,22 +58,22 @@ void mtBFS(graph_t& g, vid_t root){
         		//cout << levelCount << endl;
       	 }
      }
-    
-    
+
+
     //counts the level
     int levels[levelCount+1] = {0};
     for(int i = 0; i < (int) csr->get_vcount(); i++){
       	levels[visited[i]] = levels[visited[i]]+ 1;
     }
-    
-    
+
+
     //prints level output in form Level #: # of nodes
     for(int i = 0; i < levelCount + 1; i++){
       	cout<<"level"<< i << ": " << levels[i] <<endl;
     }
-	
 
-	
+
+
 }
 
 
@@ -87,6 +87,9 @@ void run_bfs(graph_t& g, vid_t root)
     cout << "root = " << root << endl;
     //print bfs tree here
     //i.e. how many vertex in each level
+
+		//adapted python version of BFS for C++
+		//Figured out a better way to display and count the levels
     vid_t* nbrs = csr->get_nbrs();
     vid_t* offset = csr->get_offset();
     int visited[(int) csr->get_vcount()];
@@ -98,10 +101,10 @@ void run_bfs(graph_t& g, vid_t root)
 	//cout << "sucess" << endl;
     q.push(root);
     while(!q.empty()){
-    
+
       vid_t currNode = q.front();
       //cout << currNode  << endl;
-      
+
       vid_t start = offset[currNode];
       vid_t end = offset[currNode + 1];
 
@@ -113,10 +116,10 @@ void run_bfs(graph_t& g, vid_t root)
           visited[nbrs[i]] = visited[currNode] + 1;
           //level[i] = level[currNode] + 1;
           q.push(nbrs[i]);
-          
+
           //cout << q.back()<< "blach"  << endl;
         }
-        
+
       }
       q.pop();
     }
@@ -129,19 +132,17 @@ void run_bfs(graph_t& g, vid_t root)
         //cout << levelCount << endl;
       }
     }
-    
-    
+
+
     //counts the level
     int levels[levelCount+1] = {0};
     for(int i = 0; i < (int) csr->get_vcount(); i++){
       levels[visited[i]] = levels[visited[i]]+ 1;
     }
-    
-    
+
+
     //prints level output in form Level #: # of nodes
     for(int i = 0; i < levelCount + 1; i++){
       cout<<"level"<< i << ": " << levels[i] <<endl;
     }
 }
-
-
